e5f487f9296000cd4fc4b55b62cdd13d5d271336
zchmielewska <zchmielewska@gmail.com>
2022-12-18 18:02:24 +0100

    day 18

---
+++ 18b.py
@@ -0,0 +1,88 @@
+from collections import deque
+
+# filename = "./input/18/example.txt"
+filename = "./input/18/data.txt"
+
+
+cubes = []
+with open(filename) as file:
+    for line in file:
+        row = eval(line)
+        cubes.append(row)
+
+
+def total_surface(points):
+    total = 0
+    for point in points:
+        surface = 6
+        x, y, z = point
+
+        if (x+1, y, z) in points:
+            surface -= 1
+
+        if (x-1, y, z) in points:
+            surface -= 1
+
+        if (x, y+1, z) in points:
+            surface -= 1
+
+        if (x, y-1, z) in points:
+            surface -= 1
+
+        if (x, y, z+1) in points:
+            surface -= 1
+
+        if (x, y, z-1) in points:
+            surface -= 1
+
+        total += surface
+    return(total)
+
+
+def get_neighbours(point, min_dim, max_dim):
+    neighbours = []
+    x, y, z = point
+    new_points = [(x+1, y, z), (x-1, y, z), (x, y+1, z), (x, y-1, z), (x, y, z+1), (x, y, z-1)]
+    for new_point in new_points:
+        new_x, new_y, new_z = new_point
+        if min_dim <= new_x <= max_dim and min_dim <= new_y <= max_dim and min_dim <= new_z <= max_dim:
+            neighbours.append(new_point)
+    return neighbours
+
+
+min_x = min([cube[0] for cube in cubes]) - 1
+min_y = min([cube[1] for cube in cubes]) - 1
+min_z = min([cube[2] for cube in cubes]) - 1
+max_x = max([cube[0] for cube in cubes])
+max_y = max([cube[1] for cube in cubes])
+max_z = max([cube[2] for cube in cubes])
+
+min_dim = min(min_x, min_y, min_z)
+max_dim = max(max_x, max_y, max_z)
+
+# outside air
+search_queue = deque()
+search_queue += [(min_dim, min_dim, min_dim)]
+searched = []
+
+while search_queue:
+    point = search_queue.popleft()
+    if point not in searched:
+        neighbours = get_neighbours(point, min_dim, max_dim)
+        air_neighbours = list(set(neighbours) - set(cubes))
+        search_queue += air_neighbours
+        searched.append(point)
+
+all_points = []
+for i in range(min_dim, max_dim+1):
+    for j in range(min_dim, max_dim+1):
+        for k in range(min_dim, max_dim+1):
+            all_points.append((i, j, k))
+
+outside_air = searched
+inside_air = list(set(all_points) - set(outside_air) - set(cubes))
+
+cubes_surface = total_surface(cubes)
+inside_air_surface = total_surface(inside_air)
+result = cubes_surface - inside_air_surface
+print(result)
